0466ba2bfa37b44aee50a4d4f39e7d4019b38d88
patriciafigueroa <patricia.figueroa@ucdenver.edu>
2016-04-17 23:34:30 -0600

    Submit

---
+++ Piece.cpp
@@ -0,0 +1,42 @@
+//
+//  Piece.cpp
+//  ProgrammingAssignment#4
+//
+//  Created by Patricia Figueroa on 4/10/16.
+//
+
+
+#include "Piece.h"
+
+using namespace std;
+using namespace Gaming;
+
+unsigned int Piece::__idGen = 1;
+
+Piece::Piece(const Game &g, const Position &p):__game(g)
+{
+    __id = __idGen;
+    __idGen++;
+    __finished = false;
+    __turned = false;
+    __position = p;
+}
+
+Piece::~Piece()
+{
+    
+}
+
+ostream & Gaming::operator<<(ostream &os, const Piece &piece)
+{
+    if(&piece != nullptr)
+    {
+        piece.print(os);
+    }
+    
+    else
+    {
+        os << "";
+    }
+    return os;
+}
